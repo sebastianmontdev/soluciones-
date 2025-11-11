2916b4fe8985f319f48838325235851c9f15d7fe
sebastianmontdev <sebastianmont-1199@hotmail.com>
2025-11-10 20:27:11 -0500

    Add files via upload

---
+++ ejem2.py
@@ -0,0 +1,7 @@
+def saludar (nombre = "mundo"):
+
+    print(f"hola, {nombre}!")
+
+saludar()
+saludar("pedro")
+
---
+++ sumnum.py
@@ -0,0 +1,17 @@
+def sumarNumero  (primerNumero,segundoNumero):
+
+    salidaDeAdentro = primerNumero + segundoNumero
+
+    print (f"el resultado de la suma es {salidaDeAdentro}")
+    return salidaDeAdentro
+
+
+salidaDeAfuera = "423432"
+sumarNumero(5,10)
+
+salidaDeAfuera = sumarNumero(2,3)
+numeroNuevo = int(input(0.0))
+print(sumarNumero(5,20))
+
+print(salidaDeAfuera)
+print(numeroNuevo)
